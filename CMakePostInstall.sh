4c4e794e1b17a8ec3aeb045a334ca0904c7eaed1
Manhong Dai <daimh@umich.edu>
2022-04-16 09:17:46 -0400

    fix: qmon with cmake installation

--- CMakePostInstall.sh
+++ CMakePostInstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -e
+set -eu
 cd $1/bin/lx-amd64
 ln -fs qalter qhold
 ln -fs qalter qresub
@@ -9,3 +9,5 @@ ln -fs qsh qrsh
 ln -fs qstat qselect
 echo -e "#!/usr/bin/env bash\necho 'qmake is retired due to issue #18 at github. we can revisit this issue if needed'" > qmake 
 chmod +x qmake
+cd $1/qmon/PIXMAPS/small
+mv *.xpm ../
