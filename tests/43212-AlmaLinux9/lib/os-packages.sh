584198e8a9e467f9dfea3e98ae39264af94b9c21
Manhong Dai <manhongdai@gmail.com>
2023-11-15 22:31:47 -0500

    test: 20231118, Alma 9.3

--- tests/43212-AlmaLinux9/lib/os-packages.sh
+++ tests/43212-AlmaLinux9/lib/os-packages.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 if grep -q AlmaLinux /etc/os-release
 then
-	dnf -y install https://repo.almalinux.org/almalinux/9/CRB/x86_64/os/Packages/libtirpc-devel-1.3.3-1.el9.x86_64.rpm
+	dnf -y install https://repo.almalinux.org/almalinux/9/CRB/x86_64/os/Packages/libtirpc-devel-1.3.3-2.el9.x86_64.rpm
 else
 	dnf -y install https://dl.rockylinux.org/pub/rocky/9/CRB/x86_64/os/Packages/l/libtirpc-devel-1.3.3-1.el9.x86_64.rpm
 fi
