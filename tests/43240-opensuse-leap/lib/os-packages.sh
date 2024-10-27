66ee2e64a33941433f29a2a50e7e7db36a062db4
Manhong Dai <daimh@umich.edu>
2024-10-27 12:32:27 -0400

    test: 20241027: Suse leap 15.6

--- tests/43240-opensuse-leap/lib/os-packages.sh
+++ tests/43240-opensuse-leap/lib/os-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-zypper -n rr openSUSE-Leap-15.5-1
+zypper -n rr openSUSE-Leap-15.6-1
 zypper -n update
-zypper -n addrepo http://download.opensuse.org/distribution/leap/15.5/repo/oss/ oss
+zypper -n addrepo http://download.opensuse.org/distribution/leap/15.6/repo/oss/ oss
 zypper -n install cmake gcc gcc-c++ git hwloc-devel libdb-4_8-devel libtirpc-devel libXext-devel motif-devel ncurses-devel openssl-devel pam-devel pkgconf rsync systemd-devel wget
