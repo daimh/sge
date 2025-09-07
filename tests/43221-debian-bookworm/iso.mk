97506f3e696147e775ae56cbed3dc625e67a857c
Manhong Dai <daimh@umich.edu>
2025-09-07 18:11:30 -0400

    test: 20250907, Debian 13.1, 12.12

--- tests/43221-debian-bookworm/iso.mk
+++ tests/43221-debian-bookworm/iso.mk
@@ -1,2 +1,2 @@
-Iso=debian-12.11.0-amd64-DVD-1.iso
-Url=https://cdimage.debian.org/mirror/cdimage/archive/12.11.0/amd64/iso-dvd/$(Iso)
+Iso=debian-12.12.0-amd64-DVD-1.iso
+Url=https://cdimage.debian.org/mirror/cdimage/archive/12.12.0/amd64/iso-dvd/$(Iso)
